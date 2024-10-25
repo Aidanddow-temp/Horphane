29b406f63a4d495e766aa9b13ffe6205e1e0483a
Izzy-QA <izzyqaweek@gmail.com>
2024-10-25 10:54:01 +0100

    Create UniqueSum.java

---
+++ UniqueSum.java
@@ -0,0 +1,54 @@
+public class UniqueSum {
+
+    public static void main (String[] args) {
+
+        /* first test case
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+
+         */
+        /* second test case
+        int a = 3;
+        int b = 3;
+        int c = 3;
+        */
+
+        //Third test case
+        int a = 1;
+        int b = 1;
+        int c = 2;
+
+
+        System.out.println("The sum of the unique numbers is " + sum(a,b,c));
+
+    }
+
+    public static int sum (int a, int b, int c) {
+
+        int sum;
+
+        if (a==b && b==c) {
+            sum =0;
+
+        }
+        else if (a==b) {
+            sum = c;
+
+        }
+        else if (a==c) {
+            sum =b;
+        }
+        else if (b==c) {
+            sum =a;
+        }
+        else {
+            sum = a+b+c;
+        }
+
+        return sum;
+    }
+
+
+}
