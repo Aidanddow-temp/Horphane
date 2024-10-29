953561b0fe38ec0e870ad4c92e4b3e731c430510
Iestyn Mullinor <iestynmullinor@gmail.com>
2024-10-29 11:16:35 +0000

    committed a fizz buss test fromm earlier

--- src/main/java/FizzBuzzTest.java
+++ src/main/java/FizzBuzzTest.java
@@ -1,2 +1,27 @@
+import org.junit.jupiter.api.Test;
+import sprint1.FizzBuzz;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+
+    @Test
+    public void testFizzBuzz() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        FizzBuzz.FizzBuzzPrinter(15);
+
+        System.setOut(originalOut);
+
+        // Check the output
+        assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz", outContent.toString().trim().replace("\r\n", "\n"));
+    }
+
+
 }
